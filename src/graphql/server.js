d35e0afa48054bdcd682f175bd6711e8e6951c87
Ben Galloway <git@bengalloway.io>
2020-06-30 22:23:58 +0100

    Add schema reporting to Apollo Studio.
    Perform Yarn upgrade. Bump minor version.

--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,7 +1,13 @@
 import { ApolloServer } from "apollo-server-cloud-functions";
-
 import schema from "./schema";
 import resolvers from "./resolvers/";
+
+const dotenv = require("dotenv");
+const dotenvExpand = require("dotenv-expand");
+const myEnv = dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env" : ".env.dev",
+});
+dotenvExpand(myEnv);
 
 const apolloServer = new ApolloServer({
   typeDefs: schema,
@@ -13,6 +19,10 @@ const apolloServer = new ApolloServer({
     req,
     res,
   }),
+  engine: {
+    reportSchema: true,
+    schemaReportingInitialDelayMaxMs: 1000,
+  },
 });
 
 export default apolloServer;
